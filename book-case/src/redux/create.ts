588d151b239b1f51075cad1513e0b8e91bf2ceb0
kwangminini <rhkdals7362@gmail.com>
2022-10-09 14:40:55 +0900

    init redux-saga

--- book-case/src/redux/create.ts
+++ book-case/src/redux/create.ts
@@ -1,10 +1,15 @@
 import { applyMiddleware, legacy_createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import reducer from "./modules/reducer";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from "./modules/rootSaga";
 
 const create = () => {
-    const store = legacy_createStore(reducer, composeWithDevTools(applyMiddleware()));
+    const sagaMiddleware = createSagaMiddleware();
 
+    const store = legacy_createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+    sagaMiddleware.run(rootSaga);
     return store;
 }
 
--- book-case/src/redux/modules/auth.ts
+++ book-case/src/redux/modules/auth.ts
@@ -1,6 +1,6 @@
 import { createActions, handleActions } from "redux-actions";
 
-export interface AuthState { //export 제거하면 나는 오류 확인 및 블로깅
+interface AuthState { //export 제거하면 나는 오류 확인 및 블로깅
     token : string | null;
     loading: boolean;
     error: Error | null;
@@ -35,5 +35,10 @@ const reducer = handleActions<AuthState, string>({
     })
 },initialState, {prefix});
 
+
+//saga
+export function* authSaga(){
+    
+}
 
 export default reducer;
